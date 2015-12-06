e0cfa0f1d523f43bcd64082e76e41b2a9346be03
Rannar Allorg <rannarallorg@gmail.com>
2015-12-06 11:48:33 +0200

    if user is authenticated, redirect to home

--- vorgupraxkaxpraxkax/accounts/views.py
+++ vorgupraxkaxpraxkax/accounts/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.views import login as django_login
-
+from django.http import HttpResponseRedirect
 from accounts.forms import LoginForm
 
 
 def login(request):
+
+    if request.user.is_authenticated():
+        return HttpResponseRedirect("/")
+
     response = django_login(request, template_name='accounts/login.html', authentication_form=LoginForm)
 
     return response
